30973a280b1111c3a5f8cdd400643fdd14887278
Mustafa Karadeniz <mkaradeniz2003@gmail.com>
2023-12-08 01:47:22 +0300

    updated

---
+++ cub32d.ino
@@ -0,0 +1,123 @@
+/**************************************************************************
+ * 
+ * Interfacing ESP8266 NodeMCU with ST7789 TFT display (240x240 pixel).
+ * Graphics test example.
+ * This is a free software with NO WARRANTY.
+ * https://simple-circuit.com/
+ *
+ *************************************************************************/
+
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
+
+// ST7789 TFT module connections
+#define TFT_DC 2   // TFT DC  pin is connected to NodeMCU pin D1 (GPIO5)
+#define TFT_RST 4  // TFT RST pin is connected to NodeMCU pin D2 (GPIO4)
+#define TFT_CS D8   // TFT CS  pin is connected to NodeMCU pin D8 (GPIO15)
+// initialize ST7789 TFT library with hardware SPI module
+// SCK (CLK) ---> NodeMCU pin D5 (GPIO14)
+// MOSI(DIN) ---> NodeMCU pin D7 (GPIO13)
+
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS -1 // Not connected
+#define TFT_DC 2
+#define TFT_RST 4 // Connect reset to ensure display initialises
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+float p = 3.1415926;
+
+#include <math.h>
+
+#define WIDTH 240
+#define HEIGHT 240
+#define MAP_SIZE 10
+#define FOV M_PI / 3
+#define RAY_STEP 0.01
+
+
+int game_map[MAP_SIZE][MAP_SIZE] = {
+  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  { 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 1, 0, 0, 1, 0, 0, 1 },
+  { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+};
+
+typedef struct
+{
+  double x;
+  double y;
+} Point;
+
+
+typedef struct
+{
+  Point dir;
+  Point side_dist;
+  Point delta_dist;
+  Point step;
+  Point p_map;
+
+  int side;
+  int hit;
+  double perp_wall_dist;
+} Ray;
+
+
+typedef struct
+{
+  double x;
+  double y;
+  double dir;
+} Player;
+
+typedef struct
+{
+  void *mlx;
+  void *win;
+  Player player;
+
+
+} Game;
+
+Game game;
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.print(F("Hello! ST77xx TFT Test"));
+
+  // if the display has CS pin try with SPI_MODE0
+  tft.init(240, 240, SPI_MODE2);  // Init ST7789 display 240x240 pixel
+
+  // if the screen is flipped, remove this command
+  tft.setRotation(2);
+
+  Serial.println(F("Initialized"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+  game.player.x = 2;
+  game.player.y = 2;
+  game.player.dir = M_PI / 4;
+}
+
+void loop()
+{
+  Serial.println("func");
+  raycast(&game);
+  delay(5000);
+}
---
+++ raycast.ino
@@ -0,0 +1,127 @@
+int is_hit(Ray* ray);
+void ray_step(Ray* ray);
+void calculate_perpetual(Ray* ray);
+void calculate_step(Ray* ray, Player* player);
+void init_ray(Ray* ray, Player* player, double angle);
+
+
+
+void raycast(Game* game)
+{
+	tft.fillScreen(ST77XX_BLACK);
+
+	double angle_step = FOV / WIDTH; // ray count
+	double angle = game->player.dir - FOV / 2;
+
+	for (int x = 0; x < WIDTH; x++, angle += angle_step)
+	{
+		Ray ray;
+
+		/*ray.dir.x = cos(angle);
+		ray.dir.y = sin(angle);
+
+		ray.delta_dist.x = fabs(1 / ray.dir.x);
+		ray.delta_dist.y = fabs(1 / ray.dir.y);
+
+		ray.p_map.x = (int)game->player.x;
+		ray.p_map.y = (int)game->player.y;
+
+		ray.hit = 0;*/
+    init_ray(&ray, &(game->player), angle);
+		calculate_step(&ray, &(game->player));
+		calculate_perpetual(&ray);
+
+		ray.perp_wall_dist = ray.perp_wall_dist * cos(game->player.dir - angle);
+
+		double wall_height = (1 / ray.perp_wall_dist) * HEIGHT;
+		// draw_line(game, x, wall_height, 0x00FF0000);
+
+		int start = (HEIGHT - wall_height) / 2;
+		int end = (HEIGHT + wall_height) / 2;
+
+		tft.drawLine(x, 0, x, start, ST77XX_BLUE);
+		tft.drawLine(x, start, x, end, ST77XX_RED);
+		// tft.drawLine(x, end, x, HEIGHT, ST77XX_RED);
+	}
+}
+
+void init_ray(Ray* ray, Player* player, double angle)
+{
+	ray->dir.x = cos(angle);
+	ray->dir.y = sin(angle);
+
+	ray->delta_dist.x = fabs(1 / ray->dir.x);
+	ray->delta_dist.y = fabs(1 / ray->dir.y);
+
+	ray->p_map.x = (int)player->x;
+	ray->p_map.y = (int)player->y;
+
+	ray->hit = 0;
+}
+
+int is_hit(Ray* ray)
+{
+	if (ray->p_map.x < 0 || ray->p_map.y < 0)
+		return (1);
+	if (ray->p_map.x >= MAP_SIZE || ray->p_map.y >= MAP_SIZE)
+		return (1);
+	if (game_map[(int)ray->p_map.y][(int)ray->p_map.x] == 1)
+		return (1);
+
+	return (0);
+}
+
+void ray_step(Ray* ray)
+{
+	if (ray->side_dist.x < ray->side_dist.y)
+	{
+		ray->side_dist.x += ray->delta_dist.x;
+		ray->p_map.x += ray->step.x;
+		ray->side = 0;
+	}
+	else
+	{
+		ray->side_dist.y += ray->delta_dist.y;
+		ray->p_map.y += ray->step.y;
+		ray->side = 1;
+	}
+}
+
+void calculate_perpetual(Ray* ray)
+{
+	while (ray->hit == 0)
+	{
+		ray_step(ray);
+		if (is_hit(ray))
+			ray->hit = 1;
+	}
+	if (ray->side == 0) // EAST_WEST
+		ray->perp_wall_dist = ray->side_dist.x - ray->delta_dist.x;
+	else
+		ray->perp_wall_dist = ray->side_dist.y - ray->delta_dist.y;
+}
+
+void calculate_step(Ray* ray, Player* player)
+{
+	if (ray->dir.x < 0)
+	{
+		ray->step.x = -1;
+		ray->side_dist.x = (player->x - ray->p_map.x) * ray->delta_dist.x;
+	}
+	else
+	{
+		ray->step.x = 1;
+		ray->side_dist.x = (ray->p_map.x + 1.0 - player->x) * ray->delta_dist.x;
+	}
+
+	if (ray->dir.y < 0)
+	{
+		ray->step.y = -1;
+		ray->side_dist.y = (player->y - ray->p_map.y) * ray->delta_dist.y;
+	}
+	else
+	{
+		ray->step.y = 1;
+		ray->side_dist.y = (ray->p_map.y + 1.0 - player->y) * ray->delta_dist.y;
+	}
+}
